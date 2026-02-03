f9ed4a3d35e883cf4a28154a6f75865df60f8167
ArkhamCookie <git@mail.theuntitledproject.dev>
2026-02-03 12:38:29 -0600

    feat: add tests and example files

--- src/conifg.rs
+++ src/conifg.rs
@@ -4,14 +4,14 @@ use std::path::PathBuf;
 use serde::{Deserialize, Serialize};
 use toml;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
 pub struct Bookmark {
 	pub link: String,
 	pub name: Option<String>,
 	pub description: Option<String>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
 pub struct Config {
 	pub bookmarks: Vec<Bookmark>,
 }
@@ -33,5 +33,56 @@ impl Config {
 				todo!()
 			}
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use crate::conifg::{Bookmark, Config};
+
+	#[test]
+	fn basic_bookmark_from_file() {
+		let wanted_bookmark = Bookmark {
+			link: String::from("https://arkhamcookie.com"),
+			name: Some(String::from("ArkhamCookie")),
+			description: Some(String::from("ArkhamCookie's Website")),
+		};
+		let wanted_bookmarks = vec![wanted_bookmark];
+		let wanted_config = Config {
+			bookmarks: wanted_bookmarks,
+		};
+
+		let config = Config::get(&PathBuf::from("./tests/data/basic_bookmark.toml")).unwrap();
+
+		assert_eq!(wanted_config, config);
+	}
+
+	#[test]
+	fn basic_bookmarks_from_file() {
+		let wanted_bookmark_1 = Bookmark {
+			link: String::from("https://arkhamcookie.com"),
+			name: Some(String::from("ArkhamCookie")),
+			description: Some(String::from("ArkhamCookie's Website")),
+		};
+		let wanted_bookmark_2 = Bookmark {
+			link: String::from("https://github.com"),
+			name: Some(String::from("GitHub")),
+			description: None,
+		};
+		let wanted_bookmark_3 = Bookmark {
+			link: String::from("https://youtube.com"),
+			name: None,
+			description: None,
+		};
+		let wanted_bookmarks = vec![wanted_bookmark_1, wanted_bookmark_2, wanted_bookmark_3];
+		let wanted_config = Config {
+			bookmarks: wanted_bookmarks,
+		};
+
+		let config = Config::get(&PathBuf::from("./tests/data/basic_bookmarks.toml")).unwrap();
+
+		assert_eq!(wanted_config, config)
 	}
 }
