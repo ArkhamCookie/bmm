e10a9d1805632a4691f4b11c2aa1ddfdadafe59b
ArkhamCookie <git@mail.theuntitledproject.dev>
2026-02-07 11:54:43 -0600

    docs: basic doc comments

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -1,8 +1,8 @@
 //! `bmm`'s tui components & functions
 
-///
+/// Main TUI app structure
 pub(crate) mod app;
-///
+/// Main UI code
 pub(crate) mod ui;
-///
+/// Helper functions
 pub(crate) mod utils;
